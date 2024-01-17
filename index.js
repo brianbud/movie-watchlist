ea73e62ab090074ca043ddd0ced239f5ac6df289
Brian Bud <hello@brianbud.com>
2024-01-16 22:23:52 -0500

    display a single poster and movie title

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const searchBtn = document.querySelector('#search');
 const inputEl = document.querySelector('input');
+const moviePosterEl = document.querySelector('.movie-poster');
+const movieTitleEl = document.querySelector('.movie-title');
 
 searchBtn.addEventListener('click', async () => {
   const res = await fetch(
@@ -7,4 +9,10 @@ searchBtn.addEventListener('click', async () => {
   );
   const movies = await res.json();
   console.log(movies);
+  displayMovieInfo(movies);
 });
+
+function displayMovieInfo(movies) {
+  moviePosterEl.innerHTML = `<img src=${movies.Poster}>`;
+  movieTitleEl.innerText = `${movies.Title}`;
+}
